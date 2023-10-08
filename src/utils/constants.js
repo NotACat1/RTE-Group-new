25afdfdd3edf2fa14ce9e25d75dde7973eb4b11d
NotACat1 <notacat.mail@gmail.com>
2023-10-08 22:17:39 +0400

    feat: добавление селекторов для FAQ

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const selectorsFaq = {
+  selFaq: '.faq',
+  selHeader: '.faq__header',
+  selBtn: '.faq__btn',
+  selText: '.faq__text',
+  classOpenBtn: 'faq__btn_open',
+  classOpenText: 'faq__text_open',
+};
