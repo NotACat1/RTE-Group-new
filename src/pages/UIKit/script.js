f0b1c6cc71503eb46b46c764e33861ba51fa5d86
NotACat1 <notacat.mail@gmail.com>
2023-10-12 21:26:18 +0300

    feat: добавлена работа логики страницы UI

--- src/pages/UIKit/script.js
+++ src/pages/UIKit/script.js
@@ -1,9 +1,42 @@
 import './style.scss';
 
-import { selectorsFaq } from '@utils/constants.js';
+import {
+  optionsFaq,
+  optionsForm,
+  optionsPopup,
+  optionsPopupSuccess,
+  optionsTextarea,
+  optionsTelephone,
+} from '@utils/constants.js';
 import Faq from '@components/Faq.js';
+import PopupSuccess from '@components/PopupSuccess.js';
+import Form from '@components/Form.js';
+import Textarea from '@components/Textarea.js';
+import Telephone from '@components/Telephone.js';
 
-[...document.querySelectorAll(selectorsFaq.selFaq)].forEach((item) => {
-  const newFaq = new Faq(selectorsFaq, item);
+const popupSuccess = new PopupSuccess(
+  optionsPopup,
+  optionsPopupSuccess,
+  document.querySelector(optionsPopupSuccess.selPopupSuccess),
+);
+popupSuccess.setEventListeners();
+
+[...document.querySelectorAll(optionsFaq.selFaq)].forEach((item) => {
+  const newFaq = new Faq(optionsFaq, item);
   newFaq.setEventListeners();
+});
+
+[...document.querySelectorAll(optionsForm.selForm)].forEach((item) => {
+  const newForm = new Form(optionsForm, item, popupSuccess.open.bind(popupSuccess));
+  newForm.setEventListeners();
+});
+
+[...document.querySelectorAll(optionsTextarea.selTextarea)].forEach((item) => {
+  const newTextarea = new Textarea(item);
+  newTextarea.setEventListeners();
+});
+
+[...document.querySelectorAll(optionsTelephone.selTelephone)].forEach((item) => {
+  const newTelephone = new Telephone(item);
+  newTelephone.formatting();
 });
