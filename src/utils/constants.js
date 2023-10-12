42798a5f91df58eb73da3703352a88f00b7d7133
NotACat1 <notacat.mail@gmail.com>
2023-10-12 21:20:19 +0300

    feat: добавление настроек для классов

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,40 @@
-export const selectorsFaq = {
+export const optionsFaq = {
   selFaq: '.faq',
   selHeader: '.faq__header',
   selBtn: '.faq__btn',
   selText: '.faq__text',
   classOpenBtn: 'faq__btn_open',
   classOpenText: 'faq__text_open',
+};
+
+export const optionsForm = {
+  selForm: '.form',
+  selInput: '.input__input',
+  classInputErr: 'input__input_error',
+  classTextErr: 'input__error_error',
+  selCheckbox: '.checkbox__input',
+  classCheckboxErr: 'checkbox__input_error',
+  selBtnSub: '.button.button_style_primary',
+  classBtnSubOn: 'button_style_primary',
+  classBtnSubOff: 'button_style_tertiary',
+};
+
+export const optionsPopup = {
+  selPopup: '.popup',
+  classOpen: 'popup_open',
+  selBtnClose: '.popup__btn-close',
+};
+
+export const optionsPopupSuccess = {
+  selPopupSuccess: '.popup.popup_success',
+  selTitle: '.popup__title',
+  selSubtitle: '.popup__subtitle',
+};
+
+export const optionsTextarea = {
+  selTextarea: 'textarea.input__input.input__input_textarea',
+};
+
+export const optionsTelephone = {
+  selTelephone: 'input.input__input[type="tel"]',
 };
